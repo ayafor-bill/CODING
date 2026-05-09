7a9ab234a68f8b60b7394b08d05f190244671c55
ayafor-bill <252005770+ayafor-bill@users.noreply.github.com>
2026-05-09 14:57:13 +0100

    Changed the revstr directory

--- CodingC/reverseString/revstr.c
+++ CodingC/reverseString/revstr.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_LEN 1000
-
 int main() {
-    char buffer[MAX_LEN];
-    int len = 0;
-    int c;
-
-    // Read all input into buffer
-    while ((c = getchar()) != EOF && len < MAX_LEN - 1) {
-        buffer[len++] = c;
-    }
-    buffer[len] = '\0'; // Null terminate the string
-
-    // Print the buffer in reverse order
-    for (int i = len - 1; i >= 0; i--) {
-        putchar(buffer[i]);
-    }
-
+    int c, a;
+    
+    for(int i = 0; (c = getchar()) != EOF ; i++){
+        c = a;
+    for(int j = i; (a = getchar()) != EOF ; j--){
+            putchar(a);
+        }
+    } 
+        
     return 0;
 }
